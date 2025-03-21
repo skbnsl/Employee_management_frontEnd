58ab4fec940ca7780823219f0359f237670be999
skbnsl <bansalsagar13@gmail.com>
2025-03-21 19:08:48 +0530

    configuring routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import HelloWorld from './HelloWorld'
+import {BrowserRouter, Routes} from 'react-router-dom';
 
 function App() {
 
 return (
     <>
-    <HeaderComponent/>
+     <BrowserRouter>
+      <HeaderComponent/>
+        <Routes></Routes>
       <ListEmployeeComponent/>
       <FooterComponent/>
+    </BrowserRouter>
     </>
   )
 }
